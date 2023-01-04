ab35dbcd1e2f36e798cd1c7f5c57ba6da59cd12d
maihuong112001 <mhuong112001@gmail.com>
2023-01-04 21:57:24 +0700

    template engine handlebars

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express')
+var path = require('path');
+const { engine }  = require('express-handlebars');
 const morgan = require('morgan')
 const app = express()
 const port = 3000
 
 app.use(morgan('combined'))
 
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home');
 })
 
 app.listen(port, () => {
